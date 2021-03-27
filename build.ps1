7fdb447bf697d1342493bbb3fa44b6bfbe38128c
ethanbergstrom <ethan.bergstrom@gmail.com>
2021-03-27 16:03:18 -0500

    Documentation

--- build.ps1
+++ build.ps1
@@ -12,11 +12,14 @@ $commands | ForEach-Object {
         $VerbParameters = $_.Parameters ?? @()
         $VerbOutputHandlers = $_.OutputHandlers
         $tempJson = New-TemporaryFile
-        New-CrescendoCommand -Verb $_.Verb -Noun $Noun | %{
+        New-CrescendoCommand -Verb $_.Verb -Noun $Noun | ForEach-Object {
             $_.OriginalName = $BaseOriginalName
+            # Order noun elements first, then verbs, then generic
             $_.OriginalCommandElements = ($NounOriginalCommandElements + $VerbOriginalCommandElements + $BaseOriginalCommandElements)
             $_.Description = 'PowerShell Crescendo wrapper for Chocolatey'
+            # Order noun parameters first, then verbes, then generic
             $_.Parameters = ($NounParameters + $VerbParameters + $BaseParameters)
+            # Prefer verb handlers first, then noun, then generic
             $_.OutputHandlers = ($VerbOutputHandlers ?? $NounOutputHandlers) ?? $BaseOutputHandlers
             $_
         } | ConvertTo-Json | Out-File $tempJson
