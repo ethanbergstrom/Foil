31199620e08d59e01309e9f43fb71e65bdff0bcb
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-06-03 13:53:43 +0000

    Revert "keepass-plugin-winhello broken with ChocoV2"
    This reverts commit b5491f0a1eecdf3d87a230de275a08ffd793dcd7.

--- test/Foil.chocoV2.tests.ps1
+++ test/Foil.chocoV2.tests.ps1
@@ -108,14 +108,14 @@ Describe "Chocolatey V2 pipline-based package installation and uninstallation" {
 	}
 	Context 'with dependencies' {
 		BeforeAll {
-			$package = 'cpu-z'
+			$package = 'keepass-plugin-winhello'
 		}
 
 		It 'searches for and silently installs the latest version of a package' {
 			Find-ChocoPackage -Name $package -Exact | Install-ChocoPackage -Force | Should -HaveCount 2
 		}
 		It 'detects and silently uninstalls the locally installed package just installed, along with its dependencies' {
-			Get-ChocoPackage -Name $package -LocalOnly -Exact | Uninstall-ChocoPackage -RemoveDependencies | Should -HaveCount 2
+			Get-ChocoPackage -Name $package -LocalOnly -Exact | Uninstall-ChocoPackage -RemoveDependencies | Should -HaveCount 3
 		}
 	}
 	Context 'with empty arguments' {
