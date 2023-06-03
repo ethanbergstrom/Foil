459f74943dcccb58e3a4eaab37de443e66b6f146
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-06-03 14:03:30 +0000

    ChocoV2 is no longer pre-release

--- test/Foil.chocoV2.tests.ps1
+++ test/Foil.chocoV2.tests.ps1
@@ -5,7 +5,7 @@ Describe "Chocolatey V2 test validity" {
 		$package = 'chocolatey'
 		$version = '2.0.0'
 		# Upgrade to Chocolatey v2 alpha to test the API changes
-		choco upgrade $package --pre
+		choco upgrade $package
 	}
 	It 'confirms version of Chocolatey is at least 2.0.0' {
 		Get-ChocoPackage -LocalOnly | Where-Object {$_.Name -eq $package -And $_.Version -ge $version} | Should -Not -BeNullOrEmpty
